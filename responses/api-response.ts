de5740836dca7f3b95174cea6f8e03ebcfa6da7a
deankertai <deankertai@gmail.com>
2022-12-17 02:07:05 +0000

    Allow numbers

--- responses/api-response.ts
+++ responses/api-response.ts
@@ -1,6 +1,6 @@
 export interface ApiResponse {
     statusCode: number;
-    headers?: {[key: string]: string | boolean};
+    headers?: {[key: string]: string | boolean | number};
     body?: string;
     isBase64Encoded?: boolean;
 }
